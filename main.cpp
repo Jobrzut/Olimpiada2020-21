077d5b8c60b03953f742529a860895f2f0075fdd
Julek <90630830+Jobrzut@users.noreply.github.com>
2022-03-15 20:51:27 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,12 +19,13 @@ int main() {
 		cout << "nie udalo sie otworzyc tego pliku" << endl;
 	}
 	
+		int *szereg = new int [ilosc_liczb];
+	
 	while (getline(dane, linia)) {
 		
 		switch(nr_lini) {
 			case 1: ilosc_liczb = atoi(linia.c_str()); break;
-			int *szereg = new int [ilosc_liczb];
-			case 2: for (i=0;i>ilosc_liczb; i++;) {szereg[i] = atoi(linia.c_str()); } break;
+			case 2: for (i=0; i>ilosc_liczb; i++) {szereg[i] = atoi(linia.c_str()); } break;
 		}
 		
 		nr_lini++;
@@ -33,7 +34,7 @@ int main() {
 	dane.close();
 	
 	cout << ilosc_liczb << endl;
-	cout << szereg[i] << "";
+	cout << szereg[i] << "";	
 	
 	delete [] szereg;
 }
